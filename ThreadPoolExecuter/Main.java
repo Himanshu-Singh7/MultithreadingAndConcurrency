0795348a9efbf8e5f58e89161a4c4d758fd80a9d
Himanshu <himanshusingh0608@gmail.com>
2024-06-07 00:34:56 +0530

    ThreadPoolExecuter full

--- ThreadPoolExecuter/Main.java
+++ ThreadPoolExecuter/Main.java
@@ -6,8 +6,19 @@ public class Main {
     public static void main(String[] args) {
 
         ThreadPoolExecutor executor = new ThreadPoolExecutor(2,4,10,TimeUnit.MINUTES,
-                new ArrayBlockingQueue<>(2),new CustumThreadfactory(), new ThreadPoolExecutor.DiscardPolicy());
+                new ArrayBlockingQueue<>(2),new CustumThreadfactory(), new CustumRejectedHandler());
 
+        for(int i = 1 ; i <=4 ; i++){
+            executor.submit(() -> {
+                try {
+                    Thread.sleep(5000);
+                }catch (Exception e){
+                    // handle the exception here
+                }
+                System.out.println("Task processed by : "+ Thread.currentThread().getName());
+            });
+        }
+        executor.shutdown();
     }
 }
 
@@ -18,5 +29,13 @@ class CustumThreadfactory implements ThreadFactory{
         thread.setPriority(Thread.NORM_PRIORITY);
         thread.setDaemon(false);
         return thread;
+    }
+}
+
+class  CustumRejectedHandler implements RejectedExecutionHandler{
+
+    @Override
+    public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+        System.out.println("Task rejected :" + r.toString());
     }
 }
